eb3b804a8934a4f8bb4f981f78e48dd4bcd0480f
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-05-18 03:50:34 -0300

    created header component and updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-
-import './styles/App.scss';
-import Selector from './components/Selector';
-import { useState } from 'react';
-import IOption from './interfaces/IOption';
-import OptionsList from './components/OptionsList';
+import "./styles/App.scss";
+import Selector from "./components/Selector";
+import { useState } from "react";
+import IOption from "./interfaces/IOption";
+import OptionsList from "./components/OptionsList";
+import Header from "./components/Header";
 
 function App(): JSX.Element {
   const [options, setOptions] = useState<IOption[]>([]);
   const getCurrentTimestamp = () => new Date().getTime();
-  const addOption = (option: IOption) => setOptions([...options, {...option, id:getCurrentTimestamp()}]);
-
+  const addOption = (option: IOption) =>
+    setOptions([...options, { ...option, id: getCurrentTimestamp() }]);
 
   return (
-    <div className='container'>
-      <h2>¡Selector de Opciones!</h2>
-      <Selector addOptions={addOption}/>
-      <div className='options-container'>
-        <OptionsList options={options}/>
+    <>
+      <Header />
+      <div className="container">
+        <div className="box">
+          <h2>¡Selector de Opciones!</h2>
+          <Selector addOptions={addOption} />
+          <div className="options-container">
+            <OptionsList options={options} />
+          </div>
+        </div>
       </div>
-    </div>
+      
+    </>
   );
 }
 
