24a0c9b79f219afe8ae1b0ed3ca0fac12fb1eaeb
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2024-01-06 01:51:30 -0300

    fixing header responsive with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import "./styles/App.scss";
 import Selector from "./components/Selector";
-import { useState, useRef } from "react";
+import { useState, useRef, useId } from "react";
 import IOption from "./interfaces/IOption";
 import OptionsList from "./components/OptionsList";
 import Header from "./components/Header";
-import { clear } from "console";
 import RandomOption from "./components/RandomOption";
 
 function App() {
     const [options, setOptions] = useState<IOption[]>([]);
-
     const getCurrentTimestamp = () => new Date().getTime();
-    const addOption = (option: IOption) =>
+    const addOption = (option: IOption) => {
         setOptions([...options, { ...option, id: getCurrentTimestamp() }]);
+    }
 
     return (
-        <>
+        <>  
             <Header />
-            <div className="container">
-                <div className="box">
+            <div className="containerBox">
+                <div className="container w-50 d-flex align-items-center justify-content-center flex-column" style={{border: '1px solid white'}}>
                     <h2>Random Options Selector!</h2>
                     <Selector addOptions={addOption} />
                     <RandomOption options={options} />
